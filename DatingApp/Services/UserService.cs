e852233b46224d830303170fcc84047dadd85f83
rriarys <rriarys@gmail.com>
2025-03-08 19:13:47 +0200

    before refresh

---
+++ DatingApp/Services/UserService.cs
@@ -0,0 +1,61 @@
+﻿using DatingApp.Data;
+using Microsoft.AspNetCore.Identity;
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace DatingApp.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IConfiguration _configuration;
+
+        public UserService(AppDbContext context, IPasswordHasher<User> passwordHasher, IConfiguration configuration)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUserAsync(User user)
+        {
+            user.PasswordHash = _passwordHasher.HashPassword(user, user.PasswordHash);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return IdentityResult.Success;
+        }
+        public async Task<string> AuthenticateUserAsync(User user)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+            
+            if (existingUser == null || _passwordHasher.VerifyHashedPassword(existingUser, existingUser.PasswordHash, user.PasswordHash) != PasswordVerificationResult.Success)
+            {
+                return null;
+            }
+
+            return GenerateToken(existingUser);
+        }
+
+        private string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                new[] { new Claim(ClaimTypes.Email, user.Email) },
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
